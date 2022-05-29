8b98a586c4552421abafca86d84ff33181867085
unknown <sem.shkitin@mail.ru>
2022-05-29 22:57:25 +0500

    First commit

---
+++ test4/AuthPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test4
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoginBtnClick(object sender, RoutedEventArgs e)
+        {
+            Пользователи users = new Пользователи();
+
+            using(TestEntities usersdb = new TestEntities())
+            {
+                users = usersdb.Пользователи.Where(p => p.Логин == LoginBox.Text && p.Пароль == PassBox.Text).FirstOrDefault();
+            }
+
+            if(users != null)
+            {
+                MessageBox.Show("Авторизация успешно!");
+                Manager.MainFrame.Navigate(new MenuPage());
+            }
+        }
+
+        private void RegBtnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new RegPage(null));
+        }
+    }
+}
---
+++ test4/RegPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test4
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        private Пользователи _newUser = new Пользователи();
+
+        public RegPage(Пользователи selectedUser)
+        {
+            InitializeComponent();
+
+            if (selectedUser != null)
+                _newUser = selectedUser;
+
+            DataContext = _newUser;
+        }
+
+
+        private void RegBtnClick(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_newUser.Логин))
+                errors.AppendLine("введите логин");
+            if (string.IsNullOrWhiteSpace(_newUser.Пароль))
+                errors.AppendLine("введите пароль");
+
+            if(errors.Length >0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if(_newUser.Код_Работника == 0)
+            {
+                TestEntities.GetContext().Пользователи.Add(_newUser);
+            }
+
+            try
+            {
+                TestEntities.GetContext().SaveChanges();
+                MessageBox.Show("+reg");
+                Manager.MainFrame.Navigate(new AuthPage());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        private void BackBtnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AuthPage());
+        }
+    }
+}
